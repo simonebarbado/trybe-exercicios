72e2e2942e6340fd156d49cbe8e578d98114bf6e
Simone Barbado <simonebarbado@gmail.com>
2021-11-05 18:50:39 -0300

    Exemplo variáveis

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,12 @@
+const myName = 'Simone';
+const birthCity = "São Paulo";
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Valor alterado";
