1c21d13043f01f11ef4a197a312288450fc4eb9e
Simone Barbado <simonebarbado@gmail.com>
2021-11-06 08:54:34 -0300

    Exercício 08

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-08.js
@@ -0,0 +1,9 @@
+const number1 = 5;
+const number2 = 3;
+const number3 = 1;
+
+if ((number1 % 2 === 0) || (number2 % 2 === 0) || (number3 % 2 === 0)) {
+    console.log(true);
+} else {
+    console.log(false);
+}
