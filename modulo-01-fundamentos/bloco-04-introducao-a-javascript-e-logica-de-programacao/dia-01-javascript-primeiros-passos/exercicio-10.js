9df6485b8388aa65d022ae13fc12255bd223ed47
Simone Barbado <simonebarbado@gmail.com>
2021-11-06 09:09:01 -0300

    Exercício 10 com auxílio do gabarito

--- modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-10.js
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-10.js
@@ -1,9 +1,10 @@
-const costOfProduct = 2;
+const costOfProduct = 1;
 const saleValue = 3;
 
-if (costOfProduct >= 0 && saleValue >= 0){
-    let totalProfit = (saleValue - (costOfProduct * 1,2)) * 1000;
-    console.log(totalProfit)
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
 } else {
-    console.log("Erro: os valores não podem ser negativos")
-}
+  console.log("Error, os valores não podem ser negativos");
+};
