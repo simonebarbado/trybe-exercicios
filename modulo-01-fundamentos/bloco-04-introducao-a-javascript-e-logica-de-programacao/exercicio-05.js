35d6f542002ba047fac34adc17f84250f5df43c9
Simone Barbado <simonebarbado@gmail.com>
2021-11-08 18:13:39 -0300

    Exercício 05 com ajuda do gabarito

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicio-05.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = numbers[0];
+
+for (let index in numbers) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
