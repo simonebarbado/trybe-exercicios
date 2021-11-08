f198ea134c52d6a4a753464d3c5170a924bbb111
Simone Barbado <simonebarbado@gmail.com>
2021-11-08 16:27:41 -0300

    Exercício 03

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicio-03.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index in numbers) {
+  result += numbers[index];
+}
+
+let mediaAritmetica = result / numbers.length
+
+console.log(mediaAritmetica);
