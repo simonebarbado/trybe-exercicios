f0205fdae00a25a9143a581705b81748ceb19c79
Simone Barbado <simonebarbado@gmail.com>
2021-11-08 16:29:59 -0300

    Exercício 04

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicio-04.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index in numbers) {
+  result += numbers[index];
+}
+
+let mediaAritmetica = result / numbers.length
+
+if (mediaAritmetica > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor ou igual a 20")
+}
