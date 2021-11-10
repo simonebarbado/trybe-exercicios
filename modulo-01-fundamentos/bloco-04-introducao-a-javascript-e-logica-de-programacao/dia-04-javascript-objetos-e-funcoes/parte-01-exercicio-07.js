f444eae97af9e56e6753fe4e0f20ab62101f42f9
Simone Barbado <simonebarbado@gmail.com>
2021-11-10 18:45:23 -0300

    Exercício 07

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/parte-01-exercicio-07.js
@@ -0,0 +1,20 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  leitor.livrosFavoritos.push( // Atenção aos () para funcionamento do push
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editor: 'Rocco',
+    })
+
