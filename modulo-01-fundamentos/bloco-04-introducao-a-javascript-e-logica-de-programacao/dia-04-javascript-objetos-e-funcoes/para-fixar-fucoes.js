2858845a91d3644c23ecd8a20886b146ca2db8e5
Simone Barbado <simonebarbado@gmail.com>
2021-11-10 16:05:57 -0300

    Até o exercício 05

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/para-fixar-fucoes.js
@@ -0,0 +1,89 @@
+/*
+
+Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+Um ângulo será considerado inválido se não tiver um valor positivo.
+*/
+const a = 3;
+const b = 4;
+const c = 5;
+const value = 10;
+
+// Exercício 01
+function soma(a,b) {
+    return (a + b);
+}
+
+function subtracao(a,b) {
+    return (a - b);
+}
+
+function multiplicacao(a,b) {
+    return (a * b);
+}
+
+function divisao(a,b) {
+    return (a / b);
+}
+
+function modulo(a,b) {
+    return (a % b);
+}
+
+console.log(`Os resultados são respectivamente: ${soma(a,b)}, ${subtracao(a,b)}, ${multiplicacao(a,b)}, ${divisao(a,b)}, ${modulo(a,b)}`)
+
+// Exercício 02
+function compararDoisValores(a,b) {
+    if (a > b){
+        return(`O maior valor é ${a}`);
+    } else {
+        return(`O maior valor é ${b}`);
+    }
+}
+
+console.log(compararDoisValores(a,b))
+
+// Exercício 03
+function compararTresValores(a,b,c) {
+    if (a > b && a > c){
+        return(`O maior valor é ${a}`);
+    } else if (b > a && b > c){
+        return(`O maior valor é ${b}`);
+    } else {
+        return(`O maior valor é ${c}`);
+    }
+}
+
+console.log(compararTresValores(a,b,c))
+
+// Exercício 04
+function positivoOuNegativo(value) {
+    if (value > 0){
+        return('Positive');
+    } else if (value < 0) {
+        return('Negative');
+    } else {
+        return('Zero');
+    }
+}
+
+console.log(positivoOuNegativo(value));
+
+// Exercício 05
+
+function ehTriangulo (a,b,c) {
+    let res = a + b + c;
+    let positiveAngle = a > 0 && b > 0 && c > 0; //recomendação do gabarito
+
+    if (positiveAngle) {
+        if (res === 180) {
+            console.log(true);
+        } else {
+            console.log(false);
+        }
+    } else {
+        console.log("Valor inválido")
+    }
+}
+
+console.log(ehTriangulo(a,b,c))
