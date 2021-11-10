90523bcd84f8b638549d0bbbfbb2797bb70aeb41
Simone Barbado <simonebarbado@gmail.com>
2021-11-10 15:17:43 -0300

    Para fixar

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/para-fixar.js
@@ -0,0 +1,16 @@
+// Para fixar 1
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 }
+};
+
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
+
+// Para fixar 2
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(`A jogadora Marta Silva foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`);
