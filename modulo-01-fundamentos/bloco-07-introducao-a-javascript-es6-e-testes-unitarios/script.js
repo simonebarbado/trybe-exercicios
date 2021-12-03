3ceb50edead196b188baa4458d45c007f2d1349d
Simone Barbado <simonebarbado@gmail.com>
2021-12-03 16:07:55 -0300

    Parte 1

--- modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -1,4 +1,22 @@
-function testingScope(escopo) {
+/* function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true); */
+
+/* Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+Copie o código abaixo. */
+
+const testingScope = (escopo) => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
@@ -11,3 +29,13 @@ function testingScope(escopo) {
 }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArrayBonus = array => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+}
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
