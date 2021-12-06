d3e48a912d67c2dfab4f06f0b324b5b686f8b500
Simone Barbado <simonebarbado@gmail.com>
2021-12-06 15:21:05 -0300

    Parte 03 da aula 7.2

---
+++ modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/aula-parte-02.js
@@ -0,0 +1,25 @@
+/* Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução. */
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const studentsSkills = (student) => {
+  const skills = Object.keys(student);
+  for (let i in skills) {
+    console.log(`${skills[i]}, Nível: ${student[skills[i]]} `);
+  }
+}
+studentsSkills(student1);
+studentsSkills(student2);
