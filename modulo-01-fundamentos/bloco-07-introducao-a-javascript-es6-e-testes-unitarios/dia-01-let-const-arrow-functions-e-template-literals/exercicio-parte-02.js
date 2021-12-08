707c10101ab54f4a1381cd7dff97b9a19ff53a37
Simone Barbado <simonebarbado@gmail.com>
2021-12-08 13:05:34 -0300

    Exercício 02 da parte 02 do dia 7.1

--- modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio-parte-02.js
+++ modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio-parte-02.js
@@ -6,3 +6,32 @@ const fatorialN = (n) => {
  console.log(fatorial);
 }
 fatorialN(4)
+
+const longestWord = (phrase) => {
+  let array = phrase.split(' ');
+  let longest = '';
+  for (let word of array) {
+    word.length > longest.length ? longest = word : longest = longest;
+  }
+  console.log(longest);
+}
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu");
+
+/* 
+Crie uma página que contenha:
+Um botão ao qual será associado um event listener ;
+Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+Crie um código JavaScript com a seguinte especificação:
+Não se esqueça de usar template literals
+Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+Exemplo:
+String determinada: "Tryber x aqui!"
+Parâmetro: "Bebeto"
+Retorno: "Tryber Bebeto aqui!"
+Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+JavaScript;
+HTML; ... #goTrybe".
+ */
