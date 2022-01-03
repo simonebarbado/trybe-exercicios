02697b40d68ccac84573a3649ac8cf544b4342d7
Simone Barbado <simonebarbado@gmail.com>
2022-01-03 19:41:22 -0300

    Exercício 07 dia 8.3

--- modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -62,3 +62,11 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const authorWith3DotsOnName = () => {
+  return books.filter((book) => (
+    book.author.name[1] === '.'
+    && book.author.name[4] === '.'
+    && book.author.name[7] === '.'))[0].name
+}
+
+console.log(authorWith3DotsOnName());
