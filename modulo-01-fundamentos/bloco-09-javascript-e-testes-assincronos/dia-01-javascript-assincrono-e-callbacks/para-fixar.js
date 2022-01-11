777018254af834367aa7567dcc5bde634b4f08d8
Simone Barbado <simonebarbado@gmail.com>
2022-01-11 15:02:48 -0300

    Alguns exercícios para fixar

--- modulo-01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/para-fixar.js
+++ modulo-01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/para-fixar.js
@@ -16,25 +16,63 @@
 // console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
 
 // 2 - Callback
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = (param) => {
+// const getUser = (param) => {
+//   setTimeout(() => {
+//     const user = {
+//       firstName: "Ivan",
+//       lastName: "Ivanovich",
+//       nationality: "Russian",
+//     };
+//     // Retorne a `callback` passada como parâmetro na função `getUser`
+//     // Dica: você pode manter o `console.log()`
+//     console.log(param(user));
+//   }, delay());
+// };
+
+// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// 3 - Lidando com erros em operações assíncronas
+
+// const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+// const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+// const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+// const getCountry = (onSuccess, callback) => {
+//   setTimeout(() => {
+//     const didOperationSucceed = Math.random() >= 0.5;
+//     if(didOperationSucceed) {
+//       const country = {
+//         name: "Brazil",
+//         hdi: 0.759,
+//         currency: "Real",
+//       };
+//       onSuccess(country);
+//     } else {
+//       const errorMessage = "Country could not be found";
+//       callback(errorMessage)
+//     }
+//   }, delay());
+// };
+
+// // Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+// getCountry(countryName, printErrorMessage);
+
+// // Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+// getCountry(countryCurrency, printErrorMessage);
+
+// 4 - Testes Assíncronos com Callbacks
+test('Não deveria passar!', () => {
   setTimeout(() => {
-    const user = {
-      firstName: "Ivan",
-      lastName: "Ivanovich",
-      nationality: "Russian",
-    };
-    // Retorne a `callback` passada como parâmetro na função `getUser`
-    // Dica: você pode manter o `console.log()`
-    console.log(param(user));
-  }, delay());
-};
-
-getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
-
-// 3 - 
+    expect(10).toBe(5);
+    console.log('Deveria falhar!');
+  }, 500);
+});
