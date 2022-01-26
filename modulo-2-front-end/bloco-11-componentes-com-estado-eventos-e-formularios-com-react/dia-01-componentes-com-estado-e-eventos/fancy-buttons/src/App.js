714082079882b8d2b1c744bd674f58c0489ad5e6
Simone Barbado <simonebarbado@gmail.com>
2022-01-26 16:06:40 -0300

    Aprendendo sobre bind

--- modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,37 +1,23 @@
 import React from 'react';
 import './App.css';
+
 class App extends React.Component {
   constructor() {
     super()
-    console.log('Testando se o constructor está rodando');
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  // Dentro da classe não precisa escrever function antes de uma função
-  // Será criada dentro do objeto de "this" na aba __proto__ o acesso da função
   handleClick() {
-    console.log('Clicou no 1')
-  }
-  
-  handleClick2() {
-    console.log('Clicou no 2')
-  }
-  
-  handleClick3() {
-    console.log('Clicou no 3')
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
+    console.log(this)
+    console.log('Clicou!')
   }
 
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
   render() {
-    console.log(this);
-    return (
-      <div>
-        <button onClick={this.handleClick}>Meu botão 1</button>
-        <button onClick={this.handleClick2}>Meu botão 2</button>
-        <button onClick={this.handleClick3}>Meu botão 3</button>
-      </div>
-    );
+    return <button onClick={this.handleClick}>Meu botão</button>
   }
 }
 
