f1e6fa7fdb21f8174c1d7d150a492817df81fe22
Simone Barbado <simonebarbado@gmail.com>
2022-01-26 15:57:34 -0300

    Aicionando a função react constructor

--- modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 class App extends React.Component {
+  constructor() {
+    super()
+    console.log('Testando se o constructor está rodando');
+  }
+
   // Dentro da classe não precisa escrever function antes de uma função
-    // Será criada dentro do objeto de "this" na aba __proto__ o acesso da função
-    handleClick() {
-      console.log('Clicou no 1')
-    }
-    
-    handleClick2() {
-      console.log('Clicou no 2')
-    }
-    
-    handleClick3() {
-      console.log('Clicou no 3')
-    }
+  // Será criada dentro do objeto de "this" na aba __proto__ o acesso da função
+  handleClick() {
+    console.log('Clicou no 1')
+  }
+  
+  handleClick2() {
+    console.log('Clicou no 2')
+  }
+  
+  handleClick3() {
+    console.log('Clicou no 3')
+  }
 
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
