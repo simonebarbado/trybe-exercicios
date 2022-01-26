9ed0eb1b438a296fd12e61b1a0abac3b291b3d00
Simone Barbado <simonebarbado@gmail.com>
2022-01-26 15:43:35 -0300

    Como inserir eventos e funções dentro de uma classe

--- modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import './App.css';
-
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no 1')
-}
-
-function handleClick2() {
-  console.log('Clicou no 2')
-}
-
-function handleClick3() {
-  console.log('Clicou no 3')
-}
-
 class App extends React.Component {
+  // Dentro da classe não precisa escrever function antes de uma função
+    // Será criada dentro do objeto de "this" na aba __proto__ o acesso da função
+    handleClick() {
+      console.log('Clicou no 1')
+    }
+    
+    handleClick2() {
+      console.log('Clicou no 2')
+    }
+    
+    handleClick3() {
+      console.log('Clicou no 3')
+    }
+
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
   render() {
+    console.log(this);
     return (
-      console.log(this) // Mostra tudo o que contém nessa classe
-      // <div>
-      //   <button onClick={handleClick}>Meu botão 1</button>
-      //   <button onClick={handleClick2}>Meu botão 2</button>
-      //   <button onClick={handleClick3}>Meu botão 3</button>
-      // </div>
+      <div>
+        <button onClick={this.handleClick}>Meu botão 1</button>
+        <button onClick={this.handleClick2}>Meu botão 2</button>
+        <button onClick={this.handleClick3}>Meu botão 3</button>
+      </div>
     );
   }
 }
